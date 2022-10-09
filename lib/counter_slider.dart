e516c93606f0ea6c9e469219d2426158c5409c39
Brian Craig <briancraigok@gmail.com>
2022-10-09 17:24:55 -0300

    feat: clamping

--- lib/counter_slider.dart
+++ lib/counter_slider.dart
@@ -1,4 +1,26 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
+
+Offset _onX(_CounterSliderState state)  {
+  return Offset(state.change.dx, 0);
+}
+
+Offset _onY(_CounterSliderState state)  {
+  return Offset(0, state.change.dy.clamp(0, double.infinity));
+}
+
+enum LockedOn{
+  X(_onX), Y(_onY);
+
+  const LockedOn(this.lockManipulation);
+
+  final Offset Function(_CounterSliderState state) lockManipulation;
+}
+
+bool between (double x,double min, double max){
+  return (x >= min) && (x <= max);
+}
 
 class CounterSlider extends StatefulWidget {
   const CounterSlider({super.key, required this.width, required this.height});
@@ -12,12 +34,19 @@ class CounterSlider extends StatefulWidget {
 class _CounterSliderState extends State<CounterSlider> {
   int value = 0;
 
+  Offset change = Offset.zero;
+
+  LockedOn lockedOn = LockedOn.X;
+
+  static double slideFactor = 0.4;
+
   double get bottomSize {
     return widget.height * .85;
   }
 
   @override
   Widget build(BuildContext context) {
+    var clamped = lockedOn.lockManipulation(this);
     return ConstrainedBox(
       constraints: BoxConstraints.tightFor(
         width: widget.width,
@@ -29,7 +58,8 @@ class _CounterSliderState extends State<CounterSlider> {
           clipBehavior: Clip.none,
           children: [
             Positioned(
-              left: 0,
+              left: clamped.dx * slideFactor,
+              top: clamped.dy * slideFactor,
               child: Container(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(200),
@@ -41,16 +71,36 @@ class _CounterSliderState extends State<CounterSlider> {
               ),
             ),
             Positioned(
-              left: widget.width / 2 - bottomSize / 2,
-              top: widget.height / 2 - bottomSize / 2,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(200),
-                  color: const Color.fromARGB(123, 0, 0, 0),
-                ),
-                child: SizedBox(
-                  width: bottomSize,
-                  height: bottomSize,
+              left: widget.width / 2 - bottomSize / 2 + clamped.dx,
+              top: widget.height / 2 - bottomSize / 2 + clamped.dy,
+              child: GestureDetector(
+                onPanStart: (details) {
+                  setState(() {
+                    // change = details.localPosition;
+                  });
+                },
+                onPanUpdate: (DragUpdateDetails details) {
+                  setState(() {
+                    if(change == Offset.zero){
+                      lockedOn = between(details.delta.direction, (pi* 1/4), ((pi* 3/4))) ? LockedOn.Y : LockedOn.X;
+                    }
+                    change += details.delta;
+                  });
+                },
+                onPanEnd: (details) {
+                  setState(() {
+                    change = Offset.zero;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(200),
+                    color: const Color.fromARGB(123, 0, 0, 0),
+                  ),
+                  child: SizedBox(
+                    width: bottomSize,
+                    height: bottomSize,
+                  ),
                 ),
               ),
             ),
